618f77de2db6c60670a628ffc49bb9d1d54f6eec
xhan51 <xhan51@users.noreply.github.com>
2026-03-25 13:21:00 +0800

    Site updated: 2026-03-25 13:21:00

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/03/25/esp32-robot-dog-assembly-guide/","2026/03/23/esp32-robot-dog/","2026/03/24/hello-world/","2026/03/24/openclaw-ai-novel-monetization/"];function toRandomPost(){
+    window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
+  };
